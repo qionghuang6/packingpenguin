881c879343948cfd82bb8aa944d56d4fdd5e5231
Coby Sontag <djs525@cornell.edu>
2020-12-24 18:32:38 -0500

    made render a boolean, started building item component

--- components/item.js
+++ components/item.js
@@ -1,8 +1,20 @@
-const Item = (props) => {
+import { Checkbox, FormControlLabel } from '@material-ui/core';
+
+const Item = (item) => {
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+      };
+
     return (
-        <>
-            <h3>This is a Item Component</h3>
-        </>
+        <FormControlLabel
+            control ={<Checkbox
+                checked={checked}
+                onChange={handleChange}/>}
+            label={item.name}
+        />
     )
 }
 export default Item;
+  

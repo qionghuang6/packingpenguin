64e2ec3b350495a73f7b78d734a94f190af65e11
Coby Sontag <djs525@cornell.edu>
2020-12-25 01:42:24 -0500

    nothing but comments

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,24 @@
 import Head from 'next/head'
 import { AppBar } from '@material-ui/core';
-import { createMuiTheme,  } from '@material-ui/core/styles';
 import Checklist from '../components/checklist';
 import list from '../public/defaultList.json'
+import { makeStyles } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-  overrides: {
-    // Style sheet name ⚛️
-    MuiAppBar: {
-      // Name of the rule
-      text: {
-        // Some CSS
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        border: 0,
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      },
-    },
+/*const useStyles = makeStyles({
+  headBar: {
+    // Some CSS
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
-});
-
+});*/
+// className={classes.headBar}
 export default function Home() {
+  //const classes = useStyles();
+
   return (
     <>
       <Head>
@@ -34,7 +30,7 @@ export default function Home() {
           <h1>Packing Penguin</h1>
           <h4>A Yeet by Qiong Huang and Coby Sontag</h4>
         </AppBar>
-        <Checklist name={list.name} categories={list.categories} renderPurchased={list.renderPurchased}/>
+        <Checklist name={list.name} categories={list.categories} renderPurchased={list.renderPurchased} />
       </main>
     </>
   )

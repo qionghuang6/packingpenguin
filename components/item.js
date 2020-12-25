bf9ab2cb48eea89994dfa7c73ffd68c741b566ea
Coby Sontag <djs525@cornell.edu>
2020-12-24 21:20:06 -0500

    mostly finished with item

--- components/item.js
+++ components/item.js
@@ -1,20 +1,31 @@
-import { Checkbox, FormControlLabel } from '@material-ui/core';
+import { Checkbox, FormControlLabel, FormHelperText} from '@material-ui/core';
 
-const Item = (item) => {
-    const [checked, setChecked] = React.useState(false);
+const Item = (item, renderPurchased) => {
+    const [name, setName] = React.useState(item.name)
+    const [purchased, setPurchased] = React.useState(item.isPurchased)
+    const [packed, setPacked] = React.useState(item.isPacked)
+    const [quantity, setQuantity] = React.useState(item.quantity)
+    const [notes, setNotes] = React.useState(item.notes);
 
     const handleChange = (event) => {
-        setChecked(event.target.checked);
-      };
+        setChecked({...state, [event.target.name]: event.target.checked});
+    };
 
     return (
-        <FormControlLabel
-            control ={<Checkbox
-                checked={checked}
-                onChange={handleChange}/>}
-            label={item.name}
-        />
+        <div>
+            <Checkbox 
+                disabled={!renderPurchased} 
+                checked={purchased} 
+                onChange={(e) => setChecked(e.target.checked)}/>
+            <FormControlLabel
+                control={<Checkbox
+                    checked={packed}
+                    onChange={(e) => setPacked(e.target.checked)}/>}
+                label={name} //add name changing
+            />
+            <FormHelperText>{notes}</FormHelperText> //add updating
+        </div>
     )
 }
 export default Item;
-  
+

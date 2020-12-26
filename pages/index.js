eda1cd6ea1245d3157d85bdc41d6a069789dbf7d
Coby Sontag <djs525@cornell.edu>
2020-12-26 15:32:19 -0500

    just change dheight

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ const useStyles = makeStyles({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     border: 0,
     color: 'white',
-    height: 48,
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },

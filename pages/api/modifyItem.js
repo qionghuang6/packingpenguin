9dd6f3802c43bc119638a8fd1ee66569a6f4807a
Qiong Zhou Huang <qionghuang6@gmail.com>
2020-12-27 22:11:57 -0500

    make modifyItem work properly with modification object

--- pages/api/modifyItem.js
+++ pages/api/modifyItem.js
@@ -1,13 +1,13 @@
 import { connectToDatabase } from "../../util/mongodb";
 
 export default async (req, res) => {
-    const {checkId, catId, itemId, target, targetVal} = req.body
+    const {checklistId, categoryId, itemId, target, value} = req.body
     const { db } = await connectToDatabase();
-    const checklists = db.collection("checklists")
-    const query = { id: checkId }
-    const targetKey = `categories.$[cat].items.$[item].${target}`
-    const newvalues = { $set: {targetKey: targetVal}};
-    const arrFilt = { arrayFilters: [{ 'cat.id': catId }, {'item.id': itemId}] };
+    const checklists = db.collection("checklists");
+    const query = { id: checklistId };
+    const targetKey = `categories.$[cat].items.$[item].${target}`;
+    const newvalues = { $set: {[targetKey]: value}};
+    const arrFilt = { arrayFilters: [{ 'cat.id': categoryId }, {'item.id': itemId}] };
     await checklists.updateOne(query, newvalues, arrFilt);
-    return res.status(200).end()
+    return res.status(200).end();
   };

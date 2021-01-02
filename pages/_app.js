10592833dfa0422b02485c098641a39388d3f82f
Qiong Zhou Huang <qionghuang6@gmail.com>
2021-01-01 20:57:55 -0500

    Use code from material ui example to fix server side css problems

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import createCache from '@emotion/cache';
+import theme from '../util/theme';
 
-function MyApp({ Component, pageProps }) {
+export const cache = createCache({ key: 'css', prepend: true });
 
-  return <Component {...pageProps} />
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};

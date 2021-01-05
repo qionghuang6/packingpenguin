656d5094eef218d9cd40cc5b730f653b97ce5b7d
Qiong Zhou Huang <qionghuang6@gmail.com>
2021-01-05 17:34:55 -0500

    Add reload color

--- components/category.js
+++ components/category.js
@@ -8,10 +8,11 @@ import { Delete } from '@material-ui/icons';
 const PASTELS = ['#CCD4BF', '#E7CBA9', '#EEBAB2', '#A1CDCE', '#FFCCF9', 
                 '#e8d6cf', '#F6ecf5', '#f6f6EB', '#C7CEEA', '#C4FAF8'];
 
-const Category = ({path, name: givenName, items: givenItems, renderPurchased, delCategory}) => {
+const Category = ({path, name: givenName, items: givenItems, renderPurchased, delCategory, color}) => {
     const [items, setItems] = useState(givenItems);
     const [name, setName] = usePropertyState(path, "name", givenName);
-    const [bgcolor, setBgColor] = useState(PASTELS[Math.floor(Math.random()*PASTELS.length)])
+    const givenColor = color ? color: PASTELS[Math.floor(Math.random()*PASTELS.length)];
+    const [bgColor, setBgColor] = usePropertyState(path, "color", givenColor);
 
     const addItem = async () => {
         const itemPath = path.concat([generateUniqueId()])
@@ -26,15 +27,18 @@ const Category = ({path, name: givenName, items: givenItems, renderPurchased, de
 
     return (
         <Grid item xs={12} sm={6} lg={4} xl={3} key={path[1]}>
-            <Box m={1} p={1} bgcolor={bgcolor}>
+            <Box m={1} p={1} bgcolor={bgColor}>
                 <TextField 
                             inputProps={{style: {fontSize: 24, width: name.length+ 2+"ch"}}} 
-                            // style = {{ }}
                             value={name}
                             onChange={e => setName(e.target.value)}/>
                 <Button onClick={() => delCategory(path)}><Delete/></Button>
                 {items.map(i => <Item key={i.id} path={path.concat([i.id])} item={i} renderPurchased={renderPurchased} deleteItem={deleteItem}/>)}
                 <Button variant="contained" color="secondary" onClick={addItem}>Add Item</Button>
+                <Button variant ="contained"
+                        onClick={() => setBgColor(PASTELS[Math.floor(Math.random()*PASTELS.length)])}>
+                    reload color
+                </Button>
             </Box>
         </Grid>
     )

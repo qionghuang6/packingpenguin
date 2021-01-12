ba19416150b9f8dc62229a136ea48a827a8f3c80
Qiong Zhou Huang <qionghuang6@gmail.com>
2021-01-12 13:23:58 -0500

    Change getChecklit to use promise

--- pages/api/getChecklist.js
+++ pages/api/getChecklist.js
@@ -5,28 +5,33 @@ export default async (req, res) => {
     const checklistId = req.body
     const { db } = await connectToDatabase();
     const checklists = db.collection("checklists");
-    await checklists.findOne({ id: checklistId }, (err, dbRes) => {
-        if (err) {
-            console.log(err);
-            return res.status(405).end()
-        }
-        if (!dbRes){
-            checklists.findOne({ id: "0" }, (err, defaultChecklist) => {
-                const newChecklist = JSON.parse(JSON.stringify(defaultChecklist))
-                newChecklist.id = checklistId;
-                newChecklist._id = new ObjectID();
-                checklists.insertOne(newChecklist, (e, r) => {
-                    if (e) {
-                        console.log(e);
-                        return res.status(405).end();
-                    };
-                })
-                return res.status(200).json(newChecklist);
-                }
-            )
-        } else {
-            return res.status(200).json(dbRes);
-        }
+    return new Promise(resolve => {
+        checklists.findOne({ id: checklistId }, (err, dbRes) => {
+            if (err) {
+                console.log(err);
+                res.status(405).end()
+                resolve()
+            }
+            if (!dbRes){
+                checklists.findOne({ id: "0" }, (err, defaultChecklist) => {
+                    const newChecklist = JSON.parse(JSON.stringify(defaultChecklist))
+                    newChecklist.id = checklistId;
+                    newChecklist._id = new ObjectID();
+                    checklists.insertOne(newChecklist, (e, r) => {
+                        if (e) {
+                            console.log(e);
+                            res.status(405).end();
+                            resolve();
+                        };
+                    })
+                    res.status(200).json(newChecklist);
+                    resolve();
+                    }
+                )
+            }
+            res.status(200).json(dbRes);
+            resolve()
+        })
     })
 }
 
